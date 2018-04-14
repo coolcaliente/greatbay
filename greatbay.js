b8fa10400fd1b9b03dd954797e0f38915b8c739b
coolcaliente <andreinakeller@gmail.com>
2018-04-14 13:16:44 -0500

    pseudo coded the start of this aassingment

---
+++ greatbay.js
@@ -0,0 +1,29 @@
+//install inquirer
+
+//create a database in sql
+
+// tabe information: id, name of product, current bid price, seller, highest bidder,item type, description, condition of item
+
+// connect the js file to database
+
+//create username and password in a sign up
+
+// create funtion to post new items
+
+//prompt for information about item to be sold: name of product, item type, description, condition of item, (ask to set: )current bid price
+
+// make funtion that adds this information values to database
+
+// create a function to bid on item
+
+// show list of all avilable items to select which they would like to bid on 
+
+// console asks how much user would like to bid
+
+// function that compare user bid price to the current bid price
+
+// if bid is higher, console log "success" and replace current bid price with users bid price (that should be higher)
+
+// if bid is lower or equal, console log "your bid is not higher than current bid" and kick them back to the bidding screen
+
+// 
